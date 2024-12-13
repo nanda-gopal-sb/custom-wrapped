f7362360cb8996ba20dc1fc0f5f49e444cbe56a6
Nandagopal <nandagopal.nsb@gmail.com>
2024-12-14 01:18:46 +0530

    Added files

--- src/main.js
+++ src/main.js
@@ -1,6 +1,31 @@
 import './style.css'
-document.querySelector('#app').innerHTML = `
-  <div>
-  <p>This is my way of cheering you up and saying sorry!</p>
-  </div>
-`
+let i = 0;
+const dialouge = [
+  "Sparky: Snaky boi that you",
+  "I am fine, thank you.",
+  "What are you doing?",
+  "I am learning JavaScript.",
+  "Why are you learning JavaScript?",
+  "Because I want to become a web developer."
+];
+let strAppend = embedStringInDiv(dialouge[i]);
+i++;
+
+document.querySelector('#app').innerHTML = strAppend
+function embedStringInDiv(inputString) {
+  return `
+    <div>
+      <p>${inputString}</p>
+    </div>
+  `;
+}
+document.body.onkeyup = function (e) {
+  if (i < dialouge.length) {
+    let strAppend = embedStringInDiv(dialouge[i]);
+    i++;
+    document.querySelector('#app').innerHTML = strAppend
+    const img = document.createElement('img');
+    img.src="/make-sum-happy/public/pics/sparky-d.png"
+    document.body.appendChild(img);
+  }
+}
