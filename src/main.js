cd775ffdafb03d3c32f03ddc495b97008d3c6535
Nandagopal <nandagopal.nsb@gmail.com>
2024-12-14 09:39:11 +0530

    Added pics

--- src/main.js
+++ src/main.js
@@ -1,16 +1,22 @@
 import './style.css'
 let i = 0;
+function is_touch_enabled() {
+  return ( 'ontouchstart' in window ) || 
+         ( navigator.maxTouchPoints > 0 ) ||
+         ( navigator.msMaxTouchPoints > 0 );
+}
 const dialouge = [
-  "Sparky: Snaky boi that you",
-  "I am fine, thank you.",
-  "What are you doing?",
-  "I am learning JavaScript.",
-  "Why are you learning JavaScript?",
-  "Because I want to become a web developer."
+  "Press any key to start the conversation.",
+  "Sparky: Huh, Where am I?",
+  "Snaky-Boi : ...Sparky, is that you....?",
+  "Sparky: Who tf are you, and how do you know my name?",
+  "Snaky-Boi: Dude..Its me Snaky-Boi",
+  "Sparky: Snaky-Boi? I don't know any Snaky-Boi. Ridiculous name btw. Who named you?",
+  "Snaky-Boi: Dude, did you forget everything? We are best friends. We used to play together in the park.",
 ];
 let strAppend = embedStringInDiv(dialouge[i]);
 i++;
-
+let dontAdd = false;
 document.querySelector('#app').innerHTML = strAppend
 function embedStringInDiv(inputString) {
   return `
@@ -19,13 +25,85 @@ function embedStringInDiv(inputString) {
     </div>
   `;
 }
+if (is_touch_enabled()) {
+  document.body.ontouchend = function (e) {
+    let strAppend = embedStringInDiv(dialouge[i]);
+    console.log(i);
+    console.log(dialouge[i]);
+    document.querySelector('#app').innerHTML = strAppend;
+    const img = document.createElement('img');
+    img.id = `${i}`;
+    if (i == 1) {
+      img.src = "/make-sum-happy/public/pics/sparky-confused.png";
+      img.style.transform = "scaleX(-1)";
+    }
+    if (i == 2) {
+      img.src = "/make-sum-happy/public/pics/snaky-shocked.png";
+    }
+    if (i == 3) {
+      const sparky = document.getElementById('1');
+      const snaky = document.getElementById('2');
+      sparky.src = "/make-sum-happy/public/pics/sparky-angry.png";
+      sparky.style.transform = "scaleX(-1)";
+      snaky.src = "/make-sum-happy/public/pics/snaky-afraid.png";
+      dontAdd = true;
+    }
+    if (i == 4) {
+      const snaky = document.getElementById('2');
+      snaky.src = "/make-sum-happy/public/pics/snaky-sad.png";
+    }
+    if (i == 5) {
+      const sparky = document.getElementById('1');
+      const snaky = document.getElementById('2');
+      sparky.src = "/make-sum-happy/public/pics/sparky-meh.png";
+      sparky.style.transform = "scaleX(-1)";
+      snaky.src = "/make-sum-happy/public/pics/snaky-angry.png";
+      dontAdd = true;
+    } 
+    if (!dontAdd) document.body.appendChild(img);
+    i++;
+  }
+}
+else{
+
+
 document.body.onkeyup = function (e) {
   if (i < dialouge.length) {
     let strAppend = embedStringInDiv(dialouge[i]);
-    i++;
-    document.querySelector('#app').innerHTML = strAppend
+    console.log(i);
+    console.log(dialouge[i]);
+    document.querySelector('#app').innerHTML = strAppend;
     const img = document.createElement('img');
-    img.src="/make-sum-happy/public/pics/sparky-d.png"
-    document.body.appendChild(img);
+    img.id = `${i}`;
+    if (i == 1) {
+      img.src = "/make-sum-happy/public/pics/sparky-confused.png";
+      img.style.transform = "scaleX(-1)";
+    }
+    if (i == 2) {
+      img.src = "/make-sum-happy/public/pics/snaky-shocked.png";
+    }
+    if (i == 3) {
+      const sparky = document.getElementById('1');
+      const snaky = document.getElementById('2');
+      sparky.src = "/make-sum-happy/public/pics/sparky-angry.png";
+      sparky.style.transform = "scaleX(-1)";
+      snaky.src = "/make-sum-happy/public/pics/snaky-afraid.png";
+      dontAdd = true;
+    }
+    if (i == 4) {
+      const snaky = document.getElementById('2');
+      snaky.src = "/make-sum-happy/public/pics/snaky-sad.png";
+    }
+    if (i == 5) {
+      const sparky = document.getElementById('1');
+      const snaky = document.getElementById('2');
+      sparky.src = "/make-sum-happy/public/pics/sparky-meh.png";
+      sparky.style.transform = "scaleX(-1)";
+      snaky.src = "/make-sum-happy/public/pics/snaky-angry.png";
+      dontAdd = true;
+    } 
+    if (!dontAdd) document.body.appendChild(img);
+    i++;
   }
+}
 }
