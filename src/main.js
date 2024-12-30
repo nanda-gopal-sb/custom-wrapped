4fc0a2494a648f4bc02af32cefaf5c4f3acf9f3e
Nandagopal SB <nandagopal.nsb@gmail.com>
2024-12-30 19:25:17 +0530

    added_text_over_image2

--- src/main.js
+++ src/main.js
@@ -1,1 +1,14 @@
-console.log("hello hell");
+import input from '../public/text.json';
+
+let i = 0 ;
+const text = document.getElementById('text');
+const body = document.getElementById('container');
+body.addEventListener('touchstart', function(e) {
+    text.innerHTML = input.text[i];
+    if(i === 1) {
+        i = 0;
+    }
+    else{
+        i++;
+    }
+});
