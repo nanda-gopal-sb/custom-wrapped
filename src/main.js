bb4a44d037100fbc6f765a3aac897749733f9bc5
Nandagopal SB <nandagopal.nsb@gmail.com>
2025-01-02 16:29:42 +0530

    changes

--- src/main.js
+++ src/main.js
@@ -4,10 +4,13 @@ const body = document.querySelector('body');
 const text = document.getElementById('text');
 body.addEventListener('touchstart', function (e) {
     text.innerHTML = input.text[i];
-    if (i === (input.text.length - 1)) {
-        i = 0;
+    if(i===input.text.length-1){
+        const button = document.querySelector('button');
+        button.setAttribute('onclick', 'redirect()');
     }
-    else {
+    else{
         i++;
-    }
-});
+    }   
+
+}
+);
